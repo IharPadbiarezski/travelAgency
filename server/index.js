5bb979c08a3b0dedeece73d62464fe5c29b72588
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-19 18:00:06 +0300

    feat: working with views

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 // create the new server
 const app = express();
+
+// enable Pug
+app.set('view engine', 'pug');
+
+// add the views folder into thhe project
+app.set('views', path.join(__dirname, './views'));
 
 // listen for the homepage
 app.use('/', routes());
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 module.exports = function() {
 	// homepage url
 	router.get('/', (req, res) => {
-		res.send('Index');
+		res.render('index');
 	});
 
 	// about us
 	router.get('/about', (req, res) => {
-		res.send('About Us');
+		res.render('about');
 	});
 
 	return router;
