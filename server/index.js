caa580ce95734ec52d4314bab0d146e09377def2
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-19 19:38:01 +0300

    feat: passing currentYear to the view

--- server/index.js
+++ server/index.js
@@ -14,6 +14,14 @@ app.set('views', path.join(__dirname, './views'));
 // load the public assets folder
 app.use(express.static('public'));
 
+// get the current year
+app.use((req, res, next) => {
+	const date = new Date();
+	res.locals.currentYear = date.getFullYear();
+	// console.log(res.locals);
+	return next();
+});
+
 // listen for the homepage
 app.use('/', routes());
 
